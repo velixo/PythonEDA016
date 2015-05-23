397df36ffca16565eb6e6f489216e4f039cbbad6
Vilhelm <v.lundqvist94@gmail.com>
2015-05-23 14:03:21 +0200

    Recorrected draw_square back to normal

--- lab2/draw_square.py
+++ lab2/draw_square.py
@@ -1,19 +1,9 @@
 import fix_imports	# enables importing of modules in 'PythonEDA016'
 from cs_eda016.prebuilts import *
 
-print('Creating SimpleWindow...')
 w = SimpleWindow(600, 600, 'DrawSquare')
-w.open()
-print('Creating Square...')
-sq1 = Square(0, 0, 200)
-sq2 = Square(300, 300, 200)
-print('Drawing square...')
-print()
-print('sq1')
-sq1.draw(w)
-print()
-print('sq2')
-sq2.draw(w)
+sq = Square(300, 300, 200)
+sq.draw(w)
 
 # keep the window open. should be fixed, needing to do this outside 
 # of the class itself is ugly
